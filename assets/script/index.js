444f4ffb984e7b2cd9ed2baa7d06decc81c58a68
liana valeria <liana.valeria15@gmail.com>
2021-10-02 20:51:36 +0100

    add event listener on start quiz btn and declared startquiz function

---
+++ assets/script/index.js
@@ -0,0 +1,9 @@
+const startQuizBtn = document.querySelector("#start-button");
+
+//here start quiz function
+const startQuiz = function () {
+  console.log("Quiz started");
+};
+
+//add event listener on start quiz button
+startQuizBtn.addEventListener("click", startQuiz);
