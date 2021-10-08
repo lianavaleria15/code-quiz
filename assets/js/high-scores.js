93121b85d2d15c183313dd824bbd467da9bd2200
liana valeria <liana.valeria15@gmail.com>
2021-10-08 14:42:38 +0100

    added function to create high score container div

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -4,6 +4,9 @@ const goBackBtn = document.getElementById("go-back-btn");
 //target clear scores button
 const clearScoresBtn = document.getElementById("clear-scores-btn");
 
+//target main
+const main = document.getElementById("main");
+
 //function to go back to start quiz game
 const goBackToGame = function () {
   location.href = "index.html";
@@ -13,6 +16,22 @@ const goBackToGame = function () {
 const clearHighScores = function () {
   localStorage.clear();
 };
+
+//function to create high scores container
+const createHighScoresContainer = function () {
+  //create container div
+  const containerDiv = document.createElement("div");
+  containerDiv.setAttribute("id", "high-scores-container");
+  console.log(containerDiv);
+  //create list elements
+  const highScoreElement = document.createElement("li");
+  highScoreElement.setAttribute = ("id", "score-item");
+  console.log(highScoreElement);
+  //append high scores container to main
+  containerDiv.append(highScoreElement);
+  main.append(containerDiv);
+};
+createHighScoresContainer();
 
 //add event on go back btn
 goBackBtn.addEventListener("click", goBackToGame);
