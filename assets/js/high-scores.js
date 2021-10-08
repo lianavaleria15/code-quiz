3f9890c49811264f7ebd811a62a02f26e73f371f
liana valeria <liana.valeria15@gmail.com>
2021-10-08 14:19:47 +0100

    target buttons on high-score page

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,3 +1,6 @@
 //target go back button
-
+const goBackBtn = document.getElementById("go-back-btn");
+console.log(goBackBtn);
 //target clear scores button
+const clearScoresBtn = document.getElementById("clear-scores-btn");
+console.log(clearScoresBtn);
