65ba1ed6635ff66988976ed4136428c67f6a849d
liana valeria <liana.valeria15@gmail.com>
2021-10-08 14:29:31 +0100

    added event listener on high score button and added function to clear storage and go back to main page

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,6 +1,21 @@
 //target go back button
 const goBackBtn = document.getElementById("go-back-btn");
-console.log(goBackBtn);
+
 //target clear scores button
 const clearScoresBtn = document.getElementById("clear-scores-btn");
-console.log(clearScoresBtn);
+
+//function to go back to start quiz game
+const goBackToGame = function () {
+  location.href = "index.html";
+};
+
+//function to clear high scores
+const clearHighScores = function () {
+  localStorage.clear();
+};
+
+//add event on go back btn
+goBackBtn.addEventListener("click", goBackToGame);
+
+//add event on clear scores btn
+clearScoresBtn.addEventListener("click", clearHighScores);
