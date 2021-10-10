c69594fb9153ac1c34afa18f32b82fc0374b745e
liana valeria <liana.valeria15@gmail.com>
2021-10-10 12:00:41 +0100

    added comments high scores js file

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -15,17 +15,20 @@ const goBackToGame = function () {
 //function to clear high scores
 const clearHighScores = function () {
   localStorage.clear();
-  //
+
+  //remove high scores container
   location.refresh();
 };
 
+//function to create high scores container
 const createHighScoresContainer = function (highScores) {
   //create container div
   const containerDiv = document.createElement("div");
+  //set attributes
   containerDiv.setAttribute("id", "high-scores-container");
   containerDiv.setAttribute("class", "high-scores-container");
 
-  //create a for function to display high scores
+  //function to display high scores
   for (highScore of highScores) {
     //create list elements
     const highScoreElement = document.createElement("li");
@@ -37,21 +40,22 @@ const createHighScoresContainer = function (highScores) {
   }
 
   parentContainer.append(containerDiv);
-  // return containerDiv;
 };
 
 // function to access scores in local storage
 const getScoresFromLocalStorage = function () {
-  const highScores = JSON.parse(localStorage.getItem("high_score"));
-  console.log(highScores);
+  //get high score values from local storage
+  const highScores = JSON.parse(localStorage.getItem("high_score")) ?? [];
+
   if (highScores) {
     createHighScoresContainer(highScores);
-  } else {
   }
 };
+
 window.onload = function () {
   getScoresFromLocalStorage();
 };
+
 //add event on go back btn
 goBackBtn.addEventListener("click", goBackToGame);
 
